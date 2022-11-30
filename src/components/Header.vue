94952d0bef332bb5a6abe3076633c4d280a45462
Blessing Boaslah <boaslah@IdeaPad-3-14IML05-0ff575c5.localdomain>
2022-11-30 13:44:39 +0000

    added the homeview

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div class="header">
+
+        <div></div>
+
+        <div>
+
+            <h1>Wordle</h1>
+        
+        </div>
+
+        <div></div>
+
+    </div>
+        
+</template>
+
+<script>
+
+
+export default  {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+h1{
+    text-align: center;
+}
+
+.header{
+    display: grid;
+    grid-template-columns: 33% 33% 33%;
+    border-bottom: solid 2px rgb(155, 153, 153);
+    margin: -1% 0% 0% -1%;
+}
+
+</style>
